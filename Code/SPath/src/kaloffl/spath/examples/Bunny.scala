feb51f12fc0b543b91cf8203f4a58533e3676a59
Kaloffl <lars.donner@gmx.de>
2015-12-16 11:09:35 +0100

    Simplified the bunny scene

--- Code/SPath/src/kaloffl/spath/examples/Bunny.scala
+++ Code/SPath/src/kaloffl/spath/examples/Bunny.scala
@@ -19,6 +19,7 @@ import kaloffl.spath.scene.structure.FlatObject
 import kaloffl.spath.bvh.Bvh
 import kaloffl.spath.scene.structure.HierarchicalObject
 import kaloffl.spath.math.Attenuation
+import kaloffl.spath.scene.shapes.Sphere
 
 object Bunny {
 
@@ -26,69 +27,22 @@ object Bunny {
     val display = new Display(1280, 720)
     val pathTracer = new PathTracer
 
-    val matRedDiffuse = new DiffuseMaterial(Color(0.9f, 0.1f, 0.1f))
-    val matGreenDiffuse = new DiffuseMaterial(Color(0.1f, 0.9f, 0.1f))
-    val matBlueDiffuse = new DiffuseMaterial(Color(0.1f, 0.1f, 0.9f))
-    val matYellowDiffuse = new DiffuseMaterial(Color(0.9f, 0.9f, 0.1f))
-    val matCyanDiffuse = new DiffuseMaterial(Color(0.1f, 0.9f, 0.9f))
-    val matPinkDiffuse = new DiffuseMaterial(Color(0.9f, 0.1f, 0.9f))
-    val matBlackDiffuse = new DiffuseMaterial(Color(0.1f, 0.1f, 0.1f))
-    val matWhiteDiffuse = new DiffuseMaterial(Color(0.9f, 0.9f, 0.9f))
     val matAir = new TransparentMaterial(Color(0.2f, 0.1f, 0.05f), 0.1, 0.0, 1.0)
+    val matSky = new LightMaterial(Color(1.0f, 0.95f, 0.9f), 2, Attenuation.none)
+    val matGlass = new TransparentMaterial(Color(0.9f, 0.9f, 0.9f), 1, 0, 1.7)
+    val matFloor = new DiffuseMaterial(Color(0.6f, 0.65f, 0.7f))
+    
+    val bunny = SceneNode(
+        PlyImporter.load("D:/temp/bunny_flipped.ply", Vec3d(40), Vec3d(0, -0.659748 * 2, 0)),
+        matGlass)
+        
+    val floor = SceneNode(AABB(Vec3d(-1, -0.05, -1), Vec3d(8, 0.1, 8)), matFloor)
 
-    val matWhiteLight = new LightMaterial(Color.WHITE, 2, Attenuation.none)
-
-    val matYellowGlass = new TransparentMaterial(Color(0.2f, 0.22f, 0.5f), 64, 2.0, 1.0)
-
-    val checkeredMask = new CheckeredMask(2)
-    val matBlackWhiteCheckered = new MaskedMaterial(matBlackDiffuse, matWhiteDiffuse, checkeredMask)
-
-    val objects = SceneNode(Array(
-      SceneNode(
-        new Sphere(Vec3d(-5.0f, 2.0f, 2.5f), 2),
-        matYellowDiffuse),
-
-      SceneNode(
-        new Sphere(Vec3d(-2.5f, 2.0f, -5.0f), 2.0f),
-        matCyanDiffuse),
-      SceneNode(
-        new Sphere(Vec3d(5.0f, 2.0f, 0.0f), 2.0f),
-        matPinkDiffuse),
-      SceneNode(
-        new Sphere(Vec3d(2.5f, 1.0f, 6.0f), 1.0f),
-        matWhiteDiffuse),
-      SceneNode(
-        new Sphere(Vec3d(5.0f, 1.0f, 5.0f), 1.0f),
-        matBlackDiffuse),
-
-      SceneNode(
-        PlyImporter.load("D:/temp/bunny_flipped.ply", Vec3d(40), Vec3d(0.5, -0.659748 * 2, 0)),
-        matYellowGlass),
-
-      SceneNode(
-        AABB(Vec3d(0, -0.5, 4), Vec3d(16, 1, 24)),
-        matBlackWhiteCheckered),
-      SceneNode(
-        AABB(Vec3d(0, 8.5, 4), Vec3d(16, 1, 24)),
-        matWhiteLight),
-      SceneNode(
-        AABB(Vec3d(8.5f, 4, 4), Vec3d(1, 8, 24)),
-        matRedDiffuse),
-      SceneNode(
-        AABB(Vec3d(-8.5f, 4, 4), Vec3d(1, 8, 24)),
-        matBlueDiffuse),
-      SceneNode(
-        AABB(Vec3d(0, 4, -8.5f), Vec3d(16, 8, 1)),
-        matGreenDiffuse),
-      SceneNode(
-        AABB(Vec3d(0, 4, 16.5), Vec3d(16, 8, 1)),
-        matWhiteDiffuse)))
-
-    val bunnyForward = Vec3d(-2, -1.25, -9)
+    val bunnyForward = Vec3d(-2, -1.75, -10)
     val bunnyTop = bunnyForward.cross(Vec3d.RIGHT).normalize
-    val bunnyCam = new Camera(Vec3d(0, 5, 8), bunnyForward.normalize, bunnyTop, 0.2, bunnyForward.length)
+    val bunnyCam = new Camera(Vec3d(0, 4.5, 9), bunnyForward.normalize, bunnyTop, 0.2, bunnyForward.length)
 
-    val bunnyScene = new Scene(objects, bunnyCam, matAir, matBlackDiffuse)
+    val bunnyScene = new Scene(SceneNode(Array(floor, bunny)), bunnyCam, matAir, matSky)
 
     pathTracer.render(display, bunnyScene, bounces = 12)
   }
